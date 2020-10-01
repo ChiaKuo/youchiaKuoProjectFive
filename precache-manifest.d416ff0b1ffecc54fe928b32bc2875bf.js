cf121a640f0feeff51f435677103eff605cc9b52
Jason <youchia1991@gmail.com>
2020-09-30 22:07:52 -0400

    Updates

---
+++ precache-manifest.d416ff0b1ffecc54fe928b32bc2875bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4fb529713decb4444420e7e4dea204",
+    "url": "/youchiaKuoProjectFive/index.html"
+  },
+  {
+    "revision": "61d8d6399fadd07fc012",
+    "url": "/youchiaKuoProjectFive/static/css/main.67c38aa1.chunk.css"
+  },
+  {
+    "revision": "dcb16c2f8c305c39813e",
+    "url": "/youchiaKuoProjectFive/static/js/2.16204f86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youchiaKuoProjectFive/static/js/2.16204f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d8d6399fadd07fc012",
+    "url": "/youchiaKuoProjectFive/static/js/main.54974b90.chunk.js"
+  },
+  {
+    "revision": "77d52affe48f57ab4200",
+    "url": "/youchiaKuoProjectFive/static/js/runtime-main.0e4fe17f.js"
+  },
+  {
+    "revision": "0021b161d45c006aac15278aa095b2b1",
+    "url": "/youchiaKuoProjectFive/static/media/battleBackground2.0021b161.png"
+  },
+  {
+    "revision": "3ffdf7dc1458d567f318a79ee95e6de0",
+    "url": "/youchiaKuoProjectFive/static/media/pokemonBackground.3ffdf7dc.jpg"
+  },
+  {
+    "revision": "3a977feee53b3331a3376dcd3739f591",
+    "url": "/youchiaKuoProjectFive/static/media/pokemonTitle.3a977fee.png"
+  }
+]);
